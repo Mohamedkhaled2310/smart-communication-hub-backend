8f18d4eee448f7b3abe4320adbe8aeb2adcf6ee1
Mohamedkhaled2310 <mohamedkhaled74222310@gmail.com>
2025-11-04 01:12:12 +0200

    update cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "https://smart-communication-vconnect.vercel.app/", 
     credentials: true,
   }));
 app.use(express.json());
