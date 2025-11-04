2810c642768a589b51b68371a2d8335ec932dd57
Mohamedkhaled2310 <mohamedkhaled74222310@gmail.com>
2025-11-04 16:00:59 +0200

    m

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -40,8 +40,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
-      domain: "smart-communication-hub-backend-production.up.railway.app",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // a week
     });
 
