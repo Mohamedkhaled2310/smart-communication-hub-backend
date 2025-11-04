f3fe70ba0fd17d3d860c75e774ae4b4a11257e31
Mohamedkhaled2310 <mohamedkhaled74222310@gmail.com>
2025-11-04 15:36:07 +0200

    test

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -38,9 +38,9 @@ router.post("/login", async (req, res) => {
     const token = generateToken({ userId: user.id, email: user.email });
 
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: isProduction,
-      sameSite: "lax",
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // a week
     });
 
