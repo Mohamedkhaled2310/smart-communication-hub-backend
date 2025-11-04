cec311ca628df14088a50a5c652447f029a8a3ee
Mohamedkhaled2310 <mohamedkhaled74222310@gmail.com>
2025-11-04 15:22:56 +0200

    eidt

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -40,7 +40,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // a week
     });
 
